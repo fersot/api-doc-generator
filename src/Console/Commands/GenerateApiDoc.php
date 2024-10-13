9e2118c0b289751744aa6fdb34af965aba70413f
Hember Colmenares <hemberfer@gmail.com>
2024-10-13 23:59:36 +0200

    First Commit

--- src/Console/Commands/GenerateApiDoc.php
+++ src/Console/Commands/GenerateApiDoc.php
@@ -15,18 +15,26 @@ class GenerateApiDoc extends Command
         $controller = $this->argument('controller');
         $outputPath = config('api-doc-generator.output_path');
 
+        $this->info("Output Path: {$outputPath}"); // Agrega esta línea para depuración
+
         $apiDocGenerator = new ApiDocGenerator();
 
         if ($controller) {
+            // Generar documentación para un controlador específico
             $this->info("Generating API documentation for controller: {$controller}");
             $apiDocGenerator->generateOpenApiYaml($outputPath, $controller);
             $this->info("API documentation generated at: {$outputPath}");
         } else {
+            // Generar documentación para todos los controladores
             $this->info("Generating API documentation for all controllers...");
+
+            // Obtener todos los controladores
             $controllers = $this->getAllControllers();
+
             foreach ($controllers as $controllerClass) {
                 $apiDocGenerator->generateOpenApiYaml($outputPath, $controllerClass);
             }
+
             $this->info("API documentation generated at: {$outputPath}");
         }
     }
--- src/config/api-doc-generator.php
+++ src/config/api-doc-generator.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'output_format' => 'yaml', 
-    'output_path' => base_path('docs/api-docs.yaml'), 
+    'output_path' => storage_path('api-docs/openapi.yaml'),
     'base_url' => env('APP_URL', 'http://localhost'), 
     'swagger_ui_path' => 'swagger',
 ];
